b352de2bf5b478bcd5b2a21d461bf7bc08dadc0d
Lyceum <48872358+cab-aranw@users.noreply.github.com>
2020-10-27 21:20:31 -0400

    Update

--- PA4Draft/PickTextureBrush.Designer.cs
+++ PA4Draft/PickTextureBrush.Designer.cs
@@ -32,28 +32,26 @@
             this.ImageLabel = new System.Windows.Forms.Label();
             this.comboBoxWrapModes = new System.Windows.Forms.ComboBox();
             this.buttonPickImagePickTextureBrush = new System.Windows.Forms.Button();
-            this.buttonOKPickHatchBrush = new System.Windows.Forms.Button();
-            this.buttonCancelPickHatchBrush = new System.Windows.Forms.Button();
+            this.buttonOKPickTextureBrush = new System.Windows.Forms.Button();
             this.OpenFileImage = new System.Windows.Forms.OpenFileDialog();
+            this.cancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // WrapModeLabel
             // 
             this.WrapModeLabel.AutoSize = true;
-            this.WrapModeLabel.Location = new System.Drawing.Point(135, 163);
-            this.WrapModeLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.WrapModeLabel.Location = new System.Drawing.Point(58, 73);
             this.WrapModeLabel.Name = "WrapModeLabel";
-            this.WrapModeLabel.Size = new System.Drawing.Size(144, 29);
+            this.WrapModeLabel.Size = new System.Drawing.Size(65, 13);
             this.WrapModeLabel.TabIndex = 0;
             this.WrapModeLabel.Text = "WrapModes";
             // 
             // ImageLabel
             // 
             this.ImageLabel.AutoSize = true;
-            this.ImageLabel.Location = new System.Drawing.Point(142, 272);
-            this.ImageLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.ImageLabel.Location = new System.Drawing.Point(61, 122);
             this.ImageLabel.Name = "ImageLabel";
-            this.ImageLabel.Size = new System.Drawing.Size(80, 29);
+            this.ImageLabel.Size = new System.Drawing.Size(36, 13);
             this.ImageLabel.TabIndex = 1;
             this.ImageLabel.Text = "Image";
             // 
@@ -66,60 +64,59 @@
             "TileFlipX",
             "TileFlipXY",
             "TileFlipY"});
-            this.comboBoxWrapModes.Location = new System.Drawing.Point(422, 163);
-            this.comboBoxWrapModes.Margin = new System.Windows.Forms.Padding(7);
+            this.comboBoxWrapModes.Location = new System.Drawing.Point(181, 73);
             this.comboBoxWrapModes.Name = "comboBoxWrapModes";
-            this.comboBoxWrapModes.Size = new System.Drawing.Size(643, 37);
+            this.comboBoxWrapModes.Size = new System.Drawing.Size(278, 21);
             this.comboBoxWrapModes.TabIndex = 2;
             this.comboBoxWrapModes.SelectedIndexChanged += new System.EventHandler(this.comboBoxWrapModes_SelectedIndexChanged);
             // 
             // buttonPickImagePickTextureBrush
             // 
-            this.buttonPickImagePickTextureBrush.Location = new System.Drawing.Point(422, 248);
-            this.buttonPickImagePickTextureBrush.Margin = new System.Windows.Forms.Padding(7);
+            this.buttonPickImagePickTextureBrush.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonPickImagePickTextureBrush.Location = new System.Drawing.Point(181, 111);
             this.buttonPickImagePickTextureBrush.Name = "buttonPickImagePickTextureBrush";
-            this.buttonPickImagePickTextureBrush.Size = new System.Drawing.Size(649, 51);
+            this.buttonPickImagePickTextureBrush.Size = new System.Drawing.Size(278, 23);
             this.buttonPickImagePickTextureBrush.TabIndex = 3;
             this.buttonPickImagePickTextureBrush.Text = "Choose Image";
             this.buttonPickImagePickTextureBrush.UseVisualStyleBackColor = true;
             this.buttonPickImagePickTextureBrush.Click += new System.EventHandler(this.buttonPickImagePickTextureBrush_Click);
             // 
-            // buttonOKPickHatchBrush
+            // buttonOKPickTextureBrush
             // 
-            this.buttonOKPickHatchBrush.Location = new System.Drawing.Point(110, 515);
-            this.buttonOKPickHatchBrush.Margin = new System.Windows.Forms.Padding(7);
-            this.buttonOKPickHatchBrush.Name = "buttonOKPickHatchBrush";
-            this.buttonOKPickHatchBrush.Size = new System.Drawing.Size(299, 94);
-            this.buttonOKPickHatchBrush.TabIndex = 4;
-            this.buttonOKPickHatchBrush.Text = "OK";
-            this.buttonOKPickHatchBrush.UseVisualStyleBackColor = true;
-            // 
-            // buttonCancelPickHatchBrush
-            // 
-            this.buttonCancelPickHatchBrush.Location = new System.Drawing.Point(763, 515);
-            this.buttonCancelPickHatchBrush.Margin = new System.Windows.Forms.Padding(7);
-            this.buttonCancelPickHatchBrush.Name = "buttonCancelPickHatchBrush";
-            this.buttonCancelPickHatchBrush.Size = new System.Drawing.Size(308, 94);
-            this.buttonCancelPickHatchBrush.TabIndex = 5;
-            this.buttonCancelPickHatchBrush.Text = "Cancel";
-            this.buttonCancelPickHatchBrush.UseVisualStyleBackColor = true;
+            this.buttonOKPickTextureBrush.Location = new System.Drawing.Point(47, 231);
+            this.buttonOKPickTextureBrush.Name = "buttonOKPickTextureBrush";
+            this.buttonOKPickTextureBrush.Size = new System.Drawing.Size(128, 42);
+            this.buttonOKPickTextureBrush.TabIndex = 4;
+            this.buttonOKPickTextureBrush.Text = "OK";
+            this.buttonOKPickTextureBrush.UseVisualStyleBackColor = true;
             // 
             // OpenFileImage
             // 
             this.OpenFileImage.FileName = "OpenFileImage";
             // 
+            // cancel
+            // 
+            this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancel.Location = new System.Drawing.Point(347, 231);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(112, 42);
+            this.cancel.TabIndex = 5;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            // 
             // PickTextureBrush
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AcceptButton = this.buttonOKPickTextureBrush;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1867, 1004);
-            this.Controls.Add(this.buttonCancelPickHatchBrush);
-            this.Controls.Add(this.buttonOKPickHatchBrush);
+            this.CancelButton = this.cancel;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.buttonOKPickTextureBrush);
             this.Controls.Add(this.buttonPickImagePickTextureBrush);
             this.Controls.Add(this.comboBoxWrapModes);
             this.Controls.Add(this.ImageLabel);
             this.Controls.Add(this.WrapModeLabel);
-            this.Margin = new System.Windows.Forms.Padding(7);
             this.Name = "PickTextureBrush";
             this.Text = "PickTextureBrush";
             this.ResumeLayout(false);
@@ -133,8 +130,8 @@
         private System.Windows.Forms.Label ImageLabel;
         private System.Windows.Forms.ComboBox comboBoxWrapModes;
         private System.Windows.Forms.Button buttonPickImagePickTextureBrush;
-        private System.Windows.Forms.Button buttonOKPickHatchBrush;
-        private System.Windows.Forms.Button buttonCancelPickHatchBrush;
+        private System.Windows.Forms.Button buttonOKPickTextureBrush;
         private System.Windows.Forms.OpenFileDialog OpenFileImage;
+        private System.Windows.Forms.Button cancel;
     }
 }
